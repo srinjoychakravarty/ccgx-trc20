acabbf1c644d74c9a911638d0e42a2df8c40b264
Srinjoy Chakravarty <chaxz93@gmail.com>
2019-04-22 01:09:02 +0530

    all spenders function code untested

--- contracts/ccgx.sol
+++ contracts/ccgx.sol
@@ -193,19 +193,37 @@ contract CCGX {
     }
 
     // Return array of bytes32 as solidity doesn't support returning string arrays yet
-   function getApprovedSpenders (address _benefactor) public view returns (address)
+   function getApprovedSpenders (address _benefactor) public view returns (address[])
    {
      uint i;
-     uint array_len = missHavisham[_benefactor].length;                         //checks number of approved benefactors for a given address
-     bytes[] memory gentlemen = new bytes[](array_len);                         //creates fixed length array 'gentlemen' to fit number of benefactors
+     uint array_len = missHavisham[_benefactor].length;                                         //checks number of approved benefactors for a given address
+     address[] memory spenders = new address[](array_len);                                      //creates fixed length array 'spenders' to fit number of beneficiaries
      for(i = 0; i < array_len; i++)
      {
-       gentlemen[i] = addressToBytes(missHavisham[_benefactor][i].inheritor);   //for loop inserts each benefactor address as bytes into 'gentlemen' array
+       spenders[i] = bytesToAddress(addressToBytes(missHavisham[_benefactor][i].inheritor));    //for loop inserts each benefciary address as address into 'spenders' array
      }
-     bytes memory addrInBytes = gentlemen[0];
-     address addrInAddr = bytesToAddress(addrInBytes);
-     return addrInAddr;
-  }
+     return spenders;
+   }
+
+    // function retrieveSpenders (address _benefactor) public view returns (address)
+    // {
+    //
+    // }
+    //
+    // function retrieveArsonists (address _benefactor) public view returns (address)
+    // {
+    //
+    // }
+    //
+    // function getTotalDelegatedSpend (address _benefactor) public view returns (address)
+    // {
+    //
+    // }
+    //
+    // function getTotalDelegatedBurn (address _benefactor) public view returns (address)
+    // {
+    //
+    // }
 
      //Sets allowance for the authorized '_arsonist' address to burn a maximum of `_burn` tokens on your behalf
      function approveBurn(address _arsonist, uint256 _burn) public returns (bool success)
