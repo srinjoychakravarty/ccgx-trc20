e1d8822b99f8f0afa994285711d1e4db47b251a4
Srinjoy Chakravarty <chakravarty.s@husky.neu.edu>
2019-05-15 22:35:50 +0530

    error fix

--- tronbox.js
+++ tronbox.js
@@ -1,7 +1,7 @@
 module.exports = {
   networks: {
     mainnet: {
-      privateKey: '0C7B829EE3B208D59B9746F49DF836A1881F5F98F30E10493E7AA632F87C1BE9',
+      privateKey: '',
       fullNode: "https://api.trongrid.io",
       solidityNode: "https://api.trongrid.io",
       eventServer: "https://api.trongrid.io",
